5103dae4455dcf9cbff0587f8d2534d106645e1a
Josue <josuepe03@hotmail.com>
2025-08-30 17:20:14 -0600

    add

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function build()
+    {
+        return $this->subject('Correo de prueba Resend')
+                    ->view('emails.test'); // Vista que contiene el contenido del correo
+    }
+}
---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Correo de prueba</title>
+</head>
+<body>
+    <h1>¡Hola!</h1>
+    <p>Este es un correo de prueba enviado usando Resend en Laravel.</p>
+</body>
+</html>
