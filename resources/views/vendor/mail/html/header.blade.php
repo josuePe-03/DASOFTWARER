70e640502ea7559c5d15913d0c9a0dccc98d1dd2
Josue <josuepe03@hotmail.com>
2025-08-30 15:17:42 -0600

    add

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,9 +3,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-    <img src="{{ $message->embed(public_path('images/logo.png')) }}" class="logo" alt="Mi Empresa">
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-    {!! $slot !!}
+{!! $slot !!}
 @endif
 </a>
 </td>
