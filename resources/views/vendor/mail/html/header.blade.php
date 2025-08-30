1b9166d252886e1eee422786c4b5c675c9d681ef
Josue <josuepe03@hotmail.com>
2025-08-30 15:12:37 -0600

    fixed: img correo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,12 @@
+@props(['url'])
 <tr>
-    <td class="header">
-        <a href="{{ config('app.url') }}" style="display: inline-block;">
-            <img src="{{ asset('images/logo.png') }}" class="logo" alt="Mi Empresa" style="width: 150px; height: auto;">
-        </a>
-    </td>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+    <img src="{{ $message->embed(public_path('images/logo.png')) }}" class="logo" alt="Mi Empresa">
+@else
+    {!! $slot !!}
+@endif
+</a>
+</td>
 </tr>
