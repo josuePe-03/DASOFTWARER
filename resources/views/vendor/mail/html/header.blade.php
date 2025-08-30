aa656009fedc0ef9ec0a4016b0294e1002d5613c
Josue <josuepe03@hotmail.com>
2025-08-30 14:58:21 -0600

    add: mejoras de formato correo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ config('app.url') }}" style="display: inline-block;">
+            <img src="{{ asset('images/logo.png') }}" class="logo" alt="Mi Empresa" style="width: 150px; height: auto;">
+        </a>
+    </td>
+</tr>
