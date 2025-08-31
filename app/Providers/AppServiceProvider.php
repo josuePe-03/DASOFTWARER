ce434783a9d1a4a10e7d21d21ff6c1b2d7028b99
Josue <josuepe03@hotmail.com>
2025-08-30 19:52:13 -0600

    add: configuracion global de correos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use App\Models\EmailSetting;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,25 @@ class AppServiceProvider extends ServiceProvider
         // Forzar HTTPS en producción
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+        }
+
+        // Configuración dinámica de correo desde la base de datos
+        $setting = EmailSetting::where('mailer', 'resend')->first();
+
+        if ($setting) {
+            // Configurar remitente
+            config([
+                'mail.from.address' => $setting->from_address,
+                'mail.from.name' => $setting->from_name,
+            ]);
+
+            // Setear la API Key de Resend en tiempo de ejecución
+            putenv("RESEND_API_KEY={$setting->api_key}");
+            $_ENV['RESEND_API_KEY'] = $setting->api_key;
+            $_SERVER['RESEND_API_KEY'] = $setting->api_key;
+
+            // Cambiar el mailer por default a 'resend'
+            config(['mail.default' => 'resend']);
         }
     }
 }
